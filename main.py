fee6c7696b037976623f6f21d719d93c974f43c8
Ananya Singh <singh.ananya2580@gmail.com>
2025-06-28 12:59:42 +0000

    Commit2: delete, put and exceptions (swagger/openapi docs and redoc)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import List
 from uuid import UUID, uuid4
-from fastapi import Body, FastAPI
-from models import Gender, Role, User
+from fastapi import Body, FastAPI, HTTPException
+from models import Gender, Role, User, UpdateUser
 
 app = FastAPI()
 
@@ -33,3 +33,30 @@ async def register_user(user: User = Body(...)):
     db.append(user)
     return {"id": user.id}
 #User is the entity and user is what we receive from the request body
+
+@app.delete("/api/users/{user_id}") #{user_id} is a path parameter/variable 
+async def delete_user(user_id: UUID):
+    for user in db:
+        # Check if the user with the given ID exists
+        # If it does, we break the loop
+        # If it doesn't, we continue to the next user
+        # This is a more efficient way to find the user
+        # and avoids unnecessary iterations
+        if user.id == user_id:
+            db.remove(user)
+            return {"message": "User deleted successfully"}
+        raise HTTPException(
+            status_code=404,
+            detail=f"User not found: {user_id} - User does not exist"
+        )
+    
+@app.put("/api/users/{user_id}") #{user_id} is a path parameter/variable
+async def update_user(user_id: UUID, user: UpdateUser = Body(...)):
+    for index, existing_user in enumerate(db):
+        if existing_user.id == user_id and user.first_name and user.last_name and user.gender and user.roles is not None:
+            db[index] = user
+            return {"message": "User updated successfully"}
+    raise HTTPException(
+        status_code=404,
+        detail=f"User not found: {user_id} - User does not exist"
+    )
--- models.py
+++ models.py
@@ -14,9 +14,17 @@ class Role(str, Enum):
     student = "student"
 
 class User(BaseModel):
-    id: Optional[UUID]  = Field(default_factory=uuid4) #Using uuid4() directly as a default value will generate the same UUID for all users. Use default_factory instead.    first_name: str
+    id: Optional[UUID]  = Field(default_factory=uuid4) #Using uuid4() directly as a default value will generate the same UUID for all users. Use default_factory instead.
     first_name: str
     last_name: str
     middle_name: Optional[str] = None
     gender: Gender
     roles: List[Role]
+
+class UpdateUser(BaseModel):
+    id: Optional[UUID] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    middle_name: Optional[str] = None
+    gender: Optional[Gender] = None
+    roles: Optional[List[Role]] = None
